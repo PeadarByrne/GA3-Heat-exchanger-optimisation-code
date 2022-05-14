8746bc6f95ef2d790fe5b43b9264096d4fba10cc
PeadarByrne <pb745@cam.ac.uk>
2022-05-14 15:47:51 +0100

    Added entrance pressure drop function

--- hydraulics_iteration.py
+++ hydraulics_iteration.py
@@ -1,10 +1,12 @@
 import functions as fu
 import numpy as np
 
+
 #------Hotside analysis
 
 #Guess mh
 m_h=0.45
+p_drop_h=0.7843*m_h**2 - 0.4802*m_h + 0.6598
 
 m_t=fu.m_t(m_h)
 v_t=fu.v_t(m_h)
@@ -15,12 +17,11 @@ f=(1.82*np.log10(Re_t) - 1.64)**-2
 
 dp_t = f*(fu.Lt/fu.d_i)*0.5*fu.rho*v_t**2     #pressure drop along copper tubes from friction
 sigma=fu.sigma()
-'''
-#create approximate function for kc,ke
-'''
 
-kc=0.45
-ke=0.8
+
+
+kc = -0.3952*sigma + 0.4973
+ke = 0.9773*sigma**2 -2.0738*sigma + 0.9983
 dp_e = 0.5*fu.rho*v_t**2*(kc + ke)
 dp_n = 0.5*fu.rho*v_nh**2
 
