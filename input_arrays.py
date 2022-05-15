55d873f80e4018747dcd3051600656e3f8f391d2
Alex <alex.hartlip@hotmail.co.uk>
2022-05-15 13:35:41 +0100

    addeed input arrays

---
+++ input_arrays.py
@@ -0,0 +1,8 @@
+#Contains the potential HX designs/layouts
+
+shape_array = ["sqaure","sqaure","sqaure","sqaure","sqaure","sqaure","triangular","triangular","triangular","triangular"]
+nt_array = [4,5,9,12,13,16,3,7,13,16]
+nt_cross_array = [2.828427125,3,4.242640687,4.16227766,5,5.656854249,2,3,4.464101615,5]
+l_array = [0.35,0.35,0.35,0.35,0.35,0.35,0.35,0.35,0.35,0.35]
+lt_array = [0.243,0.243,0.243,0.243,0.243,0.21875,0.243,0.243,0.243,0.21875
+Y_array = [1.67E-02,1.60E-02,1.22E-02,1.24E-02,1.07E-02,9.61E-03,2.13E-02,1.60E-02,1.17E-02,1.07E-02]
