433e6cf3837515a4c3b984014009e135083bdfcb
PeadarByrne <pb745@cam.ac.uk>
2022-05-16 20:54:58 +0100

    Added baffles to optimisation function

--- run_arrays.py
+++ run_arrays.py
@@ -5,10 +5,10 @@ import thermal_iteration as thermal
 import hydraulics_iteration as hydro
 import input_arrays as input
 
-nb = 9  #number of baffles
+
 
 #Function runs simulations for geometries in input arrays
-def run_optimisation(shape_array,nt_array,nt_cross_array,l_array,lt_array,Y_array):
+def run_optimisation(shape_array,nt_array,nt_cross_array,l_array,lt_array,Y_array,b_array):
     #check lengths of input arrays
     #check.CheckArrayLength(shape_array,nt_array,nt_cross_array,l_array,lt_array,Y_array)
     #Initialise output arrays
@@ -31,22 +31,23 @@ def run_optimisation(shape_array,nt_array,nt_cross_array,l_array,lt_array,Y_arra
         #check design is legal
         #check.CheckDesign(l,lt,nt,nt_cross,Y,nb)
 
-        
-        m_c=hydro.hydraulic_c(lt,Y,nb,N,shape)  #run cold hydrualics function
-        m_h=hydro.hydraulic_h(lt,nt)    #run hot hydraulics
-        (e_LMTD,Q_LMTD) = thermal.Thermal_LMTD(m_h,m_c,nt,nb,Y,lt,shape)    #Run LMDT analysis
-        (e_NTU,Q_NTU) = thermal.Thermal_NTU(m_h,m_c,nt,nb,Y,lt,shape)   #Run NTU analysis
-        #append results into output arrays
-        e_LMTD_array.append(e_LMTD)
-        Q_LMTD_array.append(Q_LMTD)
-        e_NTU_array.append(e_NTU)
-        Q_NTU_array.append(Q_NTU)
+        for j in range(len(b_array)):
+            nb = b_array[j]
+            m_c=hydro.hydraulic_c(lt,Y,nb,N,shape)  #run cold hydrualics function
+            m_h=hydro.hydraulic_h(lt,nt)    #run hot hydraulics
+            (e_LMTD,Q_LMTD) = thermal.Thermal_LMTD(m_h,m_c,nt,nb,Y,lt,shape)    #Run LMDT analysis
+            (e_NTU,Q_NTU) = thermal.Thermal_NTU(m_h,m_c,nt,nb,Y,lt,shape)   #Run NTU analysis
+            #append results into output arrays
+            e_LMTD_array.append(e_LMTD)
+            Q_LMTD_array.append(Q_LMTD)
+            e_NTU_array.append(e_NTU)
+            Q_NTU_array.append(Q_NTU)
         # except ValueError:
         #     e_LMTD_array.append('nope')
         #     Q_LMTD_array.append('nope')
         #     e_NTU_array.append('nope')
         #     Q_NTU_array.append('nope')
-            
+        
     
     #print results
     print('LMTD effectivenesses:')
@@ -61,5 +62,5 @@ def run_optimisation(shape_array,nt_array,nt_cross_array,l_array,lt_array,Y_arra
     return e_LMTD_array,Q_LMTD_array,e_NTU_array,Q_NTU_array
 
     
-run_optimisation(input.shape_array,input.nt_array,input.nt_cross_array,input.l_array,input.lt_array,input.Y_array)
+run_optimisation(input.shape_array,input.nt_array,input.nt_cross_array,input.l_array,input.lt_array,input.Y_array,input.b_array)
 
