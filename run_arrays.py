f6c96e51f8b5cc705f64fe1942b4535c3f1d20d9
Alex <alex.hartlip@hotmail.co.uk>
2022-05-16 16:07:34 +0100

    merge conflict?

--- run_arrays.py
+++ run_arrays.py
@@ -2,13 +2,10 @@ import functions as fu
 import validity_check as check
 import thermal_iteration as thermal
 import hydraulics_iteration as hydro
-import input_arrays as in
-
-#hydraulic_h(Lt,nt)
-#hydraulic_c(Lt,Y,nb,N,pitch_shape)
-#Thermal(m_h, m_c, nt, nb, Y, Lt, pitch_shape)
+import input_arrays as input
 
 #Function runs simulations for geometries in input arrays
-def run(shape,nt,nt_cross,l,lt,Y):
+def run_optimisation(shape,nt,nt_cross,l,lt,Y):
     count = 0
-    while count >= l
+    length = len(shape)
+    while count >= length:
