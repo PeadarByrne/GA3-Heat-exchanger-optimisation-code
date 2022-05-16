3acd1df1f453bc19fafc9b2dafce7fc1fee7c06f
PeadarByrne <pb745@cam.ac.uk>
2022-05-16 16:01:46 +0100

    Added comment of function inputs

--- run_arrays.py
+++ run_arrays.py
@@ -4,6 +4,10 @@ import thermal_iteration as thermal
 import hydraulics_iteration as hydro
 import input_arrays as in
 
+#hydraulic_h(Lt,nt)
+#hydraulic_c(Lt,Y,nb,N,pitch_shape)
+#Thermal(m_h, m_c, nt, nb, Y, Lt, pitch_shape)
+
 #Function runs simulations for geometries in input arrays
 def run(shape,nt,nt_cross,l,lt,Y):
     count = 0
