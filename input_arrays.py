e4004829df8bd87c2f27c708ac2ead51cdb7bfa9
PeadarByrne <pb745@cam.ac.uk>
2022-05-17 10:46:05 +0100

    Added a python function to calculate the  max length of copper tube based on number of tubes

--- input_arrays.py
+++ input_arrays.py
@@ -1,4 +1,5 @@
 import numpy as np
+import functions as fu
 #Contains the potential HX designs/layouts
 
 #string descriptions of pitch shape
@@ -14,7 +15,20 @@ nt_cross_array = [2.828427125,3,4.242640687,4.16227766,5,5.656854249,2,3,4.46410
 l_array = [0.35,0.35,0.35,0.35,0.35,0.35,0.35,0.35,0.35,0.35]
 
 #lengths of tubes
-lt_array = [0.243,0.243,0.243,0.243,0.243,0.21875,0.243,0.243,0.243,0.21875]
+#lt_array = [0.243,0.243,0.243,0.243,0.243,0.21875,0.243,0.243,0.243,0.21875]
+
+def lt_array_calc(nt_array):
+    l_array=[]
+    for i in nt_array:
+        l=fu.Lt_total/i
+        if l>0.243:
+            l_array.append(0.243)
+        else:
+            l_array.append(l)
+    return l_array
+
+lt_array = lt_array_calc(nt_array)   
+#print(lt_array)
 
 #pitch spacings between centres
 Y_array = [1.67E-02,1.60E-02,1.22E-02,1.24E-02,1.07E-02,9.61E-03,2.13E-02,1.60E-02,1.17E-02,1.07E-02]
