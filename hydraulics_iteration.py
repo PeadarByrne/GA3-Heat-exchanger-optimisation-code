9617a88e24c4cf279b523284c94939b85bacb426
PeadarByrne <pb745@cam.ac.uk>
2022-05-19 23:43:14 +0100

    Hotside hydraulic Newton Raphson iterator

--- hydraulics_iteration.py
+++ hydraulics_iteration.py
@@ -11,15 +11,17 @@ import numpy as np
 def hydraulic_h(Lt,nt):
     #Guess mh
     m_h=0.5
+    m_h_old = 0
+    p_calc_old = 0
     e_h_target=0.0001
     e=1
     sigma=fu.sigma(nt)
 
     counter =0
 
-    while(e>e_h_target and counter<10000):
+    while(abs(e)>e_h_target and counter<100):
         counter +=1
-        m_h_old = m_h
+        
 
         # m_hl=fu.m_L(m_h)
         # p_pump_h=-0.4713*m_h**2-0.8896*m_h + 0.6381
@@ -30,27 +32,33 @@ def hydraulic_h(Lt,nt):
 
         f=(1.82*np.log10(Re_t) - 1.64)**-2           #estimation of the friction factor in the copper tubes
 
+        #calculate pressure drop from guessed m_h
         p_t = f*(Lt/fu.d_i)*0.5*fu.rho*v_t**2     #pressure loss along copper tubes from friction
         kc = -0.3952*sigma + 0.4973                  #regression functions from excel for Re=10000 for turbulent flow
         ke = 0.9773*sigma**2 -2.0738*sigma + 0.9983  
         p_e = 0.5*fu.rho*v_t**2*(kc + ke)            #pressure loss caused by entrance exit losses into copper tubes
-        p_n = 0.5*fu.rho*v_nh**2
-
+        p_n = 0.5*fu.rho*v_nh**2    # pressure loss from nozzles entering HX
         p_h = p_t + p_e + p_n   #pressure in pascals
-        p_hb = p_h/1e5          #pressure in bar
-        m_h = (-0.3086*p_hb**2 -0.6567*p_hb + 0.5493)*fu.rho*1e-3   #regression function from excel spreadsheet correlating the pressure drop across the pump to mass flow
+        p_h_calc = p_h/1e5          #pressure in bar
 
-        e=abs(m_h-m_h_old)
-        m_h = 0.5*(m_h_old + m_h)
-        
-        if counter == 10000:
+        #m_h = (-0.3086*p_hb**2 -0.6567*p_hb + 0.5493)*fu.rho*1e-3   #regression function from excel spreadsheet correlating the pressure drop across the pump to mass flow
+        dp_calc = (p_h_calc - p_calc_old)/(m_h - m_h_old) #Calculate an approximate derivative of p_calc 
+        m_h_old = m_h   #store m_h from previous guess
+        m_hl =(fu.rho/1000)*m_h #convert to litres/s
+        p_h_pump = -0.4713*m_hl**2 - 0.8896*m_hl + 0.6598
+        dp_h_pump = -2*.4713*m_hl - 0.8896
+        m_h = m_h - (p_h_pump - p_h_calc)/(dp_h_pump - dp_calc)    #calculate new mass flow using newton raphson iteration
+        e = p_h_pump - p_h_calc #calculate error
+        p_calc_old = p_h_calc   #store previosly calculated pressure for next iteration
+      
+        if counter == 100:
             raise RuntimeError
     print(counter)
-    # check calculated m_h is within the limits of the pumps capabilities
-    # if m_h > 0.4583:
-    #     raise ValueError('outside the pumps performance limits')
-    # elif m_h < 0.0694:
-    #     raise ValueError('outside the pumps performance limits')
+    # calculated m_h is within the limits of the pumps capabilities
+    if m_h > 0.4583:    #Pump becomes unstead operating beyond this point
+        raise ValueError('outside the pumps performance limits')
+    elif m_h < 0:
+        raise ValueError('outside the pumps performance limits')
     return m_h    
 
 #------Coldside analysis
@@ -70,7 +78,7 @@ def hydraulic_c(Lt,Y,nb,N,pitch_shape):
     while(abs(e)>e_c_target and counter<=20):
 
         counter+=1
-        #calculate pressure drop from input m_c
+        #calculate pressure drop from guessed m_c
         v_sh = fu.v_sh(m_c,A_sh) 
         Re_sh = fu.Re_sh(m_c,A_sh)
         vn_c = fu.v_n(m_c)
@@ -89,24 +97,16 @@ def hydraulic_c(Lt,Y,nb,N,pitch_shape):
         m_c = m_c - (p_c_pump - p_c_calc)/(dp_c_pump - dp_calc)    #calculate new mass flow using newton raphson iteration
         e = p_c_pump - p_c_calc #calculate error
         p_calc_old = p_c_calc   #store previosly calculated pressure for next iteration
-
-        # m_c_old = m_c 
-        # de = dp_pump(m_c) - dp_calc
-        # m_c = m_c - e_func(m_c,p_c_calc)/de
-        # e = e_func(m_c,p_c_calc)
-        # p_calc_old = p_c_calc
-
-        
-
+      
         if counter == 20:
             raise RuntimeError
     print(counter)
 
-    # # check calculated m_c is within the limits of the pumps capabilities
-    # if m_c > 0.5833:
-    #     raise ValueError('outside the pumps performance limits')
-    # elif m_c < 0.1708:
-    #     raise ValueError('outside the pumps performance limits')
+    # check calculated m_c is within the limits of the pumps capabilities
+    if m_c > 0.5833:    #Pump becomes unstead operating beyond this point
+        raise ValueError('outside the pumps performance limits')
+    elif m_c < 0:
+        raise ValueError('outside the pumps performance limits')
     return m_c
 
 
