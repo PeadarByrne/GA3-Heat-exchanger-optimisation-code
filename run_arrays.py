e3281f8e47a7d93d3be01a6168a08657bbc69a6c
Alex <alex.hartlip@hotmail.co.uk>
2022-05-21 11:44:34 +0100

    run_arrays changed for new input arrays

--- run_arrays.py
+++ run_arrays.py
@@ -1,86 +1,47 @@
 from numpy import append
 import functions as fu
-import validity_check as check
-# import thermal_iteration as thermal
-# import hydraulics_iteration as hydro
+#import validity_check as check
 import single_HX_calculation as HXcalc
 import input_arrays as input
 
 
+def run_optimisation(nt_array,nb_array,passes_array,Lt_array,pitch_array,N_array)  :
+    #initialise output array
+    output_array = []
 
-#Function runs simulations for geometries in input arrays
-def run_optimisation(shape_array,nt_array,nt_cross_array,l_array,lt_array,Y_array,nb_array):
-    #check lengths of input arrays
-    check.CheckArrayLength(shape_array,nt_array,nt_cross_array,l_array,lt_array,Y_array)
-    #Initialise output arrays
-    e_LMTD_array=[]
-    Q_LMTD_array=[]
-    e_NTU_array=[]
-    Q_NTU_array=[]
+    #passes loop
+    for i in range(len(passes_array)):
+        #Iterates different passes
+        Nt=passes_array[i,0]
+        Ns=passes_array[i,1]
 
-    i_count=0
-    j_count=0
+        #nt loop
+        for j in range(len(nt_array)):
+            nt=nt_array[j]
 
-    for i in range(len(shape_array)):
-        #find datapoints for current design
-        shape=shape_array[i]
-        nt=nt_array[i]
-        nt_cross=nt_cross_array[i]
-        l=l_array[i]
-        lt=lt_array[i]
-        Y=Y_array[i]
-        N=nt    #only valid for single pass
+            #find Lt
+            if Nt==1:
+                Lt=Lt_array[j,0]
+            elif Nt==2:
+                Lt=Lt_array[j,1]
+            
+            #find Y
+            Y=pitch_array[j]
 
+            if nt>15:
+                N=4
+            else:
+                N=3
+            
+            
+            #nb loop
+            for k in range(len(nb_array)):
+                nb=nb_array[k]
 
+                #run single analysis
+                e_LMTD , Q_LMTD, e_NTU, Q_NTU = HXcalc.HX_analysis(nt,nb,Nt,Ns,Lt,Y,N)
+                #append results into one output array
+                output=[nt,nb,Nt,Ns,e_LMTD,Q_LMTD]
+                output_array.append(output)
 
-        for j in range(len(nb_array)):
-            nb = nb_array[j]
-
-            #check design is legal
-            try:
-
-                check.CheckDesign(l,lt,nt,nt_cross,Y,nb)
-
-                e_LMTD , Q_LMTD, e_NTU, Q_NTU = HXcalc.HX_analysis(nt,nb,Nt,Ns)
-                #append results into output arrays
-                e_LMTD_array.append(e_LMTD)
-                Q_LMTD_array.append(Q_LMTD)
-                e_NTU_array.append(e_NTU)
-                Q_NTU_array.append(Q_NTU)
-            except ValueError:
-                e_LMTD_array.append('nope')
-                Q_LMTD_array.append('nope')
-                e_NTU_array.append('nope')
-                Q_NTU_array.append('nope')
-                print('i=',i,' j=',j)
-
-            j_count+=1
-        i_count+=1
-    index_LMTD, e_LMTD_max = fu.return_max(e_LMTD_array)  
-    Q_LMTD_max = Q_LMTD_array[index_LMTD] 
-    index_NTU, e_NTU_max = fu.return_max(e_NTU_array)  
-    Q_NTU_max = Q_NTU_array[index_NTU] 
-    #print(shape(e_LMTD_max))
-    #print results
-    # print('LMTD effectivenesses:')
-    # print(e_LMTD_array)
-    # print('LMTD heat transfer rates:')
-    # print(Q_LMTD_array)
-    # print('NTU effectivenesses:')
-    # print(e_NTU_array)
-    # print('NTU heat transfer rates')
-    # print(Q_NTU_array)
-    
-    i_nt, i_nb =fu.index_seperator(index_LMTD,nt_array,nb_array)
-    if i_nt>5:
-        shape = "triangle"
-    else:
-        shape = "square"
-    nb=nb_array[i_nb]
-    nt=nt_array[i_nt]
-    return e_LMTD_max ,Q_LMTD_max, e_NTU_max, Q_NTU_max ,nt, nb, shape #e_LMTD_array,Q_LMTD_array,e_NTU_array,Q_NTU_array
-
-    
-e_LMTD_max, Q_LMTD_max, e_NTU_max, Q_NTU_max, i_nt, i_b, shape = run_optimisation(input.shape_array,input.nt_array,input.nt_cross_array,input.l_array,input.lt_array,input.Y_array,input.nb_array)
-
-print("e_LMTD_max: {}, Q_LMTD_max: {},e_NTU_max: {}, Q_NTU_max: {},nt: {}, nb: {}, shape: {}".format(e_LMTD_max, Q_LMTD_max,e_NTU_max, Q_NTU_max,i_nt, i_b, shape))
+           
