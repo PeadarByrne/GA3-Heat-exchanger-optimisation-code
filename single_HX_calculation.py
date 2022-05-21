51d97f9e2b2adbc1a29ea1f83cc6b3e37edd4a7a
lesterli123 <lester.li12345@gmail.com>
2022-05-21 11:21:22 +0100

    tests

--- single_HX_calculation.py
+++ single_HX_calculation.py
@@ -10,14 +10,14 @@ def HX_analysis(nt,nb,Nt,Ns):
     Y = 0.010 
     Lt = 0.2
     
-    m_c=hydro.hydraulic_c(Lt,Y,nb,N, Ns)  #run cold hydrualics function
+    m_c=hydro.hydraulic_c(Lt,Y,nb,N,Ns)  #run cold hydrualics function
     m_h=hydro.hydraulic_h(Lt,nt,Nt)    #run hot hydraulics
     e_LMTD,Q_LMTD = thermal.Thermal_LMTD(m_h,m_c,nt,nb,Y,Lt, Nt, Ns)    #Run LMDT analysis
     e_NTU,Q_NTU = thermal.Thermal_NTU(m_h,m_c,nt,nb,Y,Lt,Ns)   #Run NTU analysis
 
     return e_LMTD , Q_LMTD, e_NTU, Q_NTU
 
-e_LMTD , Q_LMTD, e_NTU, Q_NTU =HX_analysis(22, 20, 2, 2)
+e_LMTD , Q_LMTD, e_NTU, Q_NTU =HX_analysis(22, 20, 2, 1)
 print(e_LMTD , Q_LMTD, e_NTU, Q_NTU)
 
 
