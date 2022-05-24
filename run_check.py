12af39713ff3b52127fca5990d74b9399683c19f
Alex <alex.hartlip@hotmail.co.uk>
2022-05-24 15:55:09 +0100

    set up final design analyses

--- run_check.py
+++ run_check.py
@@ -1,4 +1,12 @@
 import validity_check as check
+import single_HX_calculation as HXcalc
 
 
-check.CheckMass(0.232,14,15,1,1)
+design1=[12,7,2,2,0.238,,]
+design2=[16,6,2,2,0.207,,]
+design3=[14,16,2,1,0.238,,]
+design4=[16,13,2,1,0.207,,]
+design5=[18,5,2,2,0.182,,]
+design6=[18,11,2,1,0.182,,]
+
+output1=HXcalc.HX_analysis(design1[0],design1[1],design1[2],design1[3],design1[4],design1[5],design1[6])
