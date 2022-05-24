d7976c3ea03e6523b1d9915ebf9e8cdeee8f31fc
Alex <alex.hartlip@hotmail.co.uk>
2022-05-24 16:05:31 +0100

    just needs Y and N for the 6 potential designs

--- run_check.py
+++ run_check.py
@@ -10,3 +10,15 @@ design5=[18,5,2,2,0.182,,]
 design6=[18,11,2,1,0.182,,]
 
 output1=HXcalc.HX_analysis(design1[0],design1[1],design1[2],design1[3],design1[4],design1[5],design1[6])
+output2=HXcalc.HX_analysis(design2[0],design2[1],design2[2],design2[3],design2[4],design2[5],design2[6])
+output3=HXcalc.HX_analysis(design3[0],design3[1],design3[2],design3[3],design3[4],design3[5],design3[6])
+output4=HXcalc.HX_analysis(design4[0],design4[1],design4[2],design4[3],design4[4],design4[5],design4[6])
+output5=HXcalc.HX_analysis(design5[0],design5[1],design5[2],design5[3],design5[4],design5[5],design5[6])
+output6=HXcalc.HX_analysis(design6[0],design6[1],design6[2],design6[3],design6[4],design6[5],design6[6])
+
+print('Q1 = ',output1[1])
+print('Q2 = ',output2[1])
+print('Q3 = ',output3[1])
+print('Q4 = ',output4[1])
+print('Q5 = ',output5[1])
+print('Q6 = ',output6[1])
