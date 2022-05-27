f3fda8aca0f44feb28e407ab0def9466d03a7c32
Alex <alex.hartlip@hotmail.co.uk>
2022-05-27 12:55:37 +0100

    fixed thermal different Reynolds, not using equivalent HX any more

--- run_check.py
+++ run_check.py
@@ -11,7 +11,7 @@ design6=[20,6,4,2,0.163,0.01,3,58e-3,23e-3,0.25]
 
 output1=HXcalc.single_HX_analysis(design1[0],design1[1],design1[2],design1[3],design1[4],design1[5],design1[6],design1[7],design1[8],design1[9])
 output2=HXcalc.single_HX_analysis(design2[0],design2[1],design2[2],design2[3],design2[4],design2[5],design2[6],design2[7],design2[8],design2[9])
-output3=HXcalc.single_HX_analysis(design3[0],design3[1],design3[2],design3[3],design3[4],design3[5],design3[6],design3[7],design3[8],design3[9])
+output3=HXcalc.single_HX_analysis_special(design3[0],design3[1],design3[2],design3[3],design3[4],design3[5],design3[6],design3[7],design3[8],design3[9])
 output4=HXcalc.single_HX_analysis(design4[0],design4[1],design4[2],design4[3],design4[4],design4[5],design4[6],design4[7],design4[8],design4[9])
 output5=HXcalc.single_HX_analysis(design5[0],design5[1],design5[2],design5[3],design5[4],design5[5],design5[6],design5[7],design5[8],design5[9])
 output6=HXcalc.single_HX_analysis(design6[0],design6[1],design6[2],design6[3],design6[4],design6[5],design6[6],design6[7],design6[8],design6[9])
