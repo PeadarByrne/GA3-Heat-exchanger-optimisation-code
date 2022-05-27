a62287ad5a61688287a8cfe859e5fcb4f84c0ac7
Alex <alex.hartlip@hotmail.co.uk>
2022-05-27 14:30:12 +0100

    optimiser wrks for bug fix now

--- single_HX_calculation.py
+++ single_HX_calculation.py
@@ -5,9 +5,9 @@ import functions as fu
 
 
 
-def HX_analysis(nt,nb,Nt,Ns,Lt,Y,N,header_gap_n,header_gap):
+def HX_analysis(nt,nb,Nt,Ns,Lt,Y,N,HGn=57.5e-3,HG=57.5e-3):
     m_c=hydro.hydraulic_c(Lt,Y,nb,N,Ns)  #run cold hydrualics function
-    m_h=hydro.hydraulic_h(Lt,nt,Nt,header_gap_n,header_gap)    #run hot hydraulics
+    m_h=hydro.hydraulic_h(Lt,nt,Nt,HGn,HG)    #run hot hydraulics
     e_LMTD,Q_LMTD = thermal.Thermal_LMTD(m_h,m_c,nt,nb,Y,Lt, Nt, Ns)    #Run LMDT analysis
     e_NTU,Q_NTU = thermal.Thermal_NTU(m_h,m_c,nt,nb,Y,Lt,Ns)   #Run NTU analysis
 
